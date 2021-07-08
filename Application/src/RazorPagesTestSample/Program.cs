52eee9e96bbee970e815a7ef030132dce47fd22b
nicolalgallacher <nigallac@microsoft.com>
2021-07-08 14:29:01 +0100

    Change in comment to trigger action

--- Application/src/RazorPagesTestSample/Program.cs
+++ Application/src/RazorPagesTestSample/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using RazorPagesTestSample.Data;
 
-//Comment added to check if actions work
+//
 namespace RazorPagesTestSample
 {
     public class Program
