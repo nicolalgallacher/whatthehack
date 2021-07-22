c49a269858bfb5e7255c7b8e28bdd5904d16caf2
nicolalgallacher <nigallac@microsoft.com>
2021-07-22 11:19:53 +0100

    added with line:

--- Application/src/RazorPagesTestSample/Program.cs
+++ Application/src/RazorPagesTestSample/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using RazorPagesTestSample.Data;
 
-//    
+//    update
 namespace RazorPagesTestSample
 {
     public class Program
