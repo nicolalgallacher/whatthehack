4349100e49e2ffa76d78c565dd44d13878beef16
nicolalgallacher <nigallac@microsoft.com>
2021-07-28 13:11:14 +0100

    Update to remove login

--- Application/src/RazorPagesTestSample/Program.cs
+++ Application/src/RazorPagesTestSample/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using RazorPagesTestSample.Data;
 
-//    
+//    Update
 namespace RazorPagesTestSample
 {
     public class Program
